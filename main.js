380606b15fccba04e96211f65464731054194758
Carlos <carloskeko80@gmail.com>
2021-05-03 19:01:29 +0200

    Creacion nodejs

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-var conexion= mysql.createConnection({
+var conexion = mysql.createConnection({
     host : 'localhost',
     database : 'TeamDue',
     user : 'Carlos',
@@ -26,6 +26,16 @@ server.listen(8080, function() {
     console.log('Servidor corriendo en http://localhost:8080');
 });
 
+app.get('/' , (req , res)=>{
+
+   res.send('Hola');
+
+})
+
 io.on('connection', function(socket) {
     console.log('Un cliente se ha conectado');
+    io.on("login", (data) => {
+
+
+    })
 });

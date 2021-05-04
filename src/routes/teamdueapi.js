94ae91ea0f8c6baa2e9dbc844f2047be90d0f012
CarlosKeko <carloskeko80@gmail.com>
2021-05-04 18:44:29 +0200

    api

--- src/routes/teamdueapi.js
+++ src/routes/teamdueapi.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const conexion = require("../database.js");
 
 router.get('/', (req, res) => {
+    console.log("Entrando por get /");
     conexion.query("SELECT * FROM Usuarios", (err, rows, fields)=> {
         if (!err) {
             res.json(rows);
@@ -15,6 +16,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+    console.log("Entrando por get /id");
     const { id } = req.params;
     conexion.query("SELECT * FROM Usuarios WHERE id = ?", [id], (err, rows, fields)=> {
         if (!err) {
@@ -28,6 +30,7 @@ router.get('/:id', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+    console.log("Entrando por post /login");
     const { user, password } = req.body;
     conexion.query("SELECT * FROM Usuarios WHERE usuario = ? AND password = ? OR correo = ? AND password = ?", [user, password, user, password], (err, rows, fields) => {
         if (!err) {

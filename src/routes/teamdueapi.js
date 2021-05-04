b382bfec9e4ea4f5703ef4af1e9513ab10379c3f
CarlosKeko <carloskeko80@gmail.com>
2021-05-04 20:07:02 +0200

    api

--- src/routes/teamdueapi.js
+++ src/routes/teamdueapi.js
@@ -45,9 +45,9 @@ router.get('/files/:id', (req, res) => {
 
 router.post('/login', (req, res) => {
     console.log("Entrando por POST /login");
-    const { user, password } = req.body;
-    console.log(user, "y", password);
-    conexion.query("SELECT * FROM Usuarios WHERE usuario = ? AND password = ? OR correo = ? AND password = ?", [user, password, user, password], (err, rows, fields) => {
+    const { usuario, password } = req.body;
+    console.log(usuario, "y", password);
+    conexion.query("SELECT * FROM Usuarios WHERE usuario = ? AND password = ? OR correo = ? AND password = ?", [usuario, password, usuario, password], (err, rows, fields) => {
         if (!err) {
             res.json(rows[0]);
 
@@ -60,11 +60,19 @@ router.post('/login', (req, res) => {
 
 router.post('/register', (req, res) => {
     console.log("Entrando por POST /register");
-    const { nombre, apellidos, correo, usuario, password, fecha_registro, premium } = req.body;
+    const { nombre, apellidos, correo, usuario, password } = req.body;
     conexion.query("SELECT * FROM Usuarios WHERE usuario = ? OR correo = ?", [usuario, correo], (err, rows, fields) => {
         if (!err) {
             if (rows < 1) {
+                conexion.query("INSERT INTO Usuarios (nombre, apellidos, correo, usuario, password, premium, fecha_registro) VALUES (?, ?, ?, ?, ?, ?, ?)", [nombre, apellidos, correo, usuario, password, 0, now()], (err, rows, fields) => {
+                    if (!err) {
+                        res.send(201, "Created");
 
+                    }else {
+                        res.send(400, err.message);
+                    
+                    }
+                });
 
             }else {
                 res.send(409, "Duplicate");

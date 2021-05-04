14b3b9c6dd7b66854c481808b45e209971d78e0e
CarlosKeko <carloskeko80@gmail.com>
2021-05-04 23:33:19 +0200

    api

--- src/routes/teamdueapi.js
+++ src/routes/teamdueapi.js
@@ -51,7 +51,7 @@ router.post('/login', (req, res) => {
     conexion.query("SELECT * FROM Usuarios WHERE usuario = ? OR correo = ? LIMIT 1", [usuario, usuario], (err, rows, fields) => {
         if (!err) {
             console.log(rows[0]);
-            if (rows > 0) {
+            if (rows != undefined) {
                 console.log("entra");
                 const pass = rows[0]["password"];
                 hashPasswordIsSame(pass, password).then(isSame => {

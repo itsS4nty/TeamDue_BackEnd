c79cfe59ba703287fac171a7be17156c60c55d5b
CarlosKeko <carloskeko80@gmail.com>
2021-05-04 12:30:00 +0200

    session handler

--- main.js
+++ main.js
@@ -2,29 +2,34 @@
 const connImport = require('./conexion.js');
 const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {cors:{origin: "*",}});
-const {Server} = require("socket.io"), server = new Server(8000);
+// const {Server} = require("socket.io"), server = new Server(8000);
 
 
 // Variables
 var conexion = connImport.crearConexion();
-
+const session_handler = require('io-session-handler').from(io, { timeout: 5000 });
 
 // Funciones
-io.on("connection", (socket) => {
-    console.log("Nueva conexion: ", socket.remoteAddress);
+// io.on("connection", (socket) => {
+//     console.log("Nueva conexion:", socket.id);
 
-    socket.on("login", (data) => {
-        conexion.query("SELECT * FROM Usuarios WHERE usuario LIKE '" + data.user + "' AND password LIKE '" + data.password + "'", function (err, result, fields) {
-            if (err) {
-                console.error('Error de consulta: ' + err.stack);
-                return;
-            }
+//     socket.on("login", (data) => {
+//         conexion.query("SELECT * FROM Usuarios WHERE usuario LIKE '" + data.user + "' AND password LIKE '" + data.password + "'", function (err, result, fields) {
+//             if (err) {
+//                 console.error('Error de consulta: ' + err.stack);
+//                 return;
+//             }
 
-            console.log(result);
-            // socket.emit
-        });
+//             console.log(result);
+//             // socket.emit
+//         });
 
-    })
+//     })
+// });
+ 
+session_handler.connectionListener((connection) => {
+    console.log(connection);
 });
-  
+
+
 httpServer.listen(8080);

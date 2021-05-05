090a94a5d23a00a76cabaa0d934fc1a52426caf4
Carlos Keko <carloskeko80@gmail.com>
2021-05-05 20:29:25 +0200

    sequelize

--- migrations/20210505165033-create-configuracion-usuario.js
+++ migrations/20210505165033-create-configuracion-usuario.js
@@ -3,6 +3,10 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('ConfiguracionUsuarios', {
       id: {
+        references: {
+          model: 'Usuarios',
+          key: 'id'
+        },
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,

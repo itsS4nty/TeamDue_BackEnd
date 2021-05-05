80bf73176bec5d2a8fdd5b4dd558615ff8f42045
Carlos Keko <carloskeko80@gmail.com>
2021-05-05 18:46:31 +0200

    sequelize

---
+++ migrations/20210505164017-create-usuarios.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      apellidos: {
+        type: Sequelize.STRING
+      },
+      correo: {
+        type: Sequelize.STRING
+      },
+      usuario: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      fecha_registro: {
+        type: Sequelize.DATE
+      },
+      premium: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Usuarios');
+  }
+};

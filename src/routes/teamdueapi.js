736350cadc0f3c087cff8cbf8cfe374da20d3d95
CarlosKeko <carloskeko80@gmail.com>
2021-05-06 17:55:54 +0200

    api

--- src/routes/teamdueapi.js
+++ src/routes/teamdueapi.js
@@ -102,7 +102,7 @@ router.post('/createFile', (req, res) => {
     console.log("Entrando por POST /createFile");
     const { nombre:nombreInp, tipo:tipoInp, UsuarioId:UsuarioIdInp } = req.body;
 
-    db.Archivos.findOne({where: { [Op.and]: [{UsuarioId:UsuarioIdInp, nombre:nombreInp, tipo:tipoInp}] }}).then((findedArchivo) => {
+    db.Archivos.findOne({where: { [Op.and]: [{UsuarioId:UsuarioIdInp}, {nombre:nombreInp}, {tipo:tipoInp}] }}).then((findedArchivo) => {
         if (findedArchivo === null) {
             db.Archivos.create({
                 nombre: nombreInp,

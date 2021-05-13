e58cfff96c7aedc400ef42faf480e73e7ec0c51a
CarlosKeko <carloskeko80@gmail.com>
2021-05-13 15:55:22 +0200

    Solucionando nodemon

--- src/main.js
+++ src/main.js
@@ -107,4 +107,13 @@ io.on("connection", (socket) => {
     });
 
 });
+
+process.once('SIGUSR2', function () {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+  
+  process.on('SIGINT', function () {
+    // this is only called on ctrl+c, not restart
+    process.kill(process.pid, 'SIGINT');
+  });
  

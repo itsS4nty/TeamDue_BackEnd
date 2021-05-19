8f8b9c2c507f2f6a67fe5a6664b6826d7e013f09
CarlosKeko <carloskeko80@gmail.com>
2021-05-19 03:36:32 +0200

    cambiando modelo

--- migrations/20210505164017-create-usuarios.js
+++ migrations/20210505164017-create-usuarios.js
@@ -17,7 +17,7 @@ module.exports = {
         type: Sequelize.STRING
       },      
       apellido2: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.STRING
       },
       correo: {

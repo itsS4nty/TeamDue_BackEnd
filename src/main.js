2498aa7367084c323c68984057a7a8671bf90583
CarlosKeko <carloskeko80@gmail.com>
2021-05-25 18:46:18 +0200

    pruebas

--- src/main.js
+++ src/main.js
@@ -184,7 +184,7 @@ io.on("connection", (socket) => {
         for (var j = 0; j < gameRooms.length; j++) {
             if (gameRooms[j].roomKey == data.idRoom && gameRooms[j].nombreAdmin == data.usuario) {
                 console.log(gameRooms[j].administrator);
-                if (!gameRooms[j].administrator.find(socket.id)) {
+                if (!(gameRooms[j].administrator).find(socket.id)) {
                     gameRooms[j].administrator.push(socket.id); 
                 }
                 console.log(gameRooms[j].administrator);

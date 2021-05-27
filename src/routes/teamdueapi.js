134c1d12385119e598e66ed31ba8edc2758d83d2
santi <82sao82@gmail.com>
2021-05-27 12:42:13 +0200

    Info\

--- src/routes/teamdueapi.js
+++ src/routes/teamdueapi.js
@@ -17,9 +17,8 @@ const app = express();
 const { validateToken } = require("../jwt/validate.js");
 
 app.set("llave", config.keyMaster);
-app.use(bodyParser.json({limit: '200mb'}));
-app.use(bodyParser.urlencoded({limit: '200mb', extended: true}));
-app.use(bodyParser.text({ limit: '200mb' }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 router.use(cors());
 
 router.get('/files/:id', (req, res) => {
